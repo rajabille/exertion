eaa241f5b8faf4c3518a009f9bd6fb3538d8c5df
Rajabille <36477604+Rajabille@users.noreply.github.com>
2018-02-26 00:38:16 +0530

    Change to HW.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World I am from DevOps domain V 1.4");
+		get("/hello", (req, res) -> "Hello World I am from DevOps domain V 1.5");
 	}
 }
